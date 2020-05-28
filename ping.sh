b6405ae7abc5dd42646a881a9ed98c74b6401b72
Daniel Volrab <volrisek@gmail.com>
2020-05-28 14:27:12 +0200

    úprava ping.sh, pridani read

--- ping.sh
+++ ping.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-PING=$1
-PING_adv=$( ping "$1" -w1 | grep "ttl"  | cut -d " " -f7 | cut -d "=" -f2)
+echo "Zadej kam chceš pingnout"
+read PING
+
+echo "Ping...."
+PING_adv=$( ping $PING -w1 | grep "ttl"  | cut -d " " -f7 | cut -d "=" -f2)
 
 if [[ ${PING_adv} -gt 0 ]]; then
     echo "Internet dostupný"
