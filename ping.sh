d9bc7382ded98fb42af5381e042f32771bf02aea
Daniel Volrab <volrisek@gmail.com>
2020-05-28 10:46:36 +0200

    úprava ping.sh

--- ping.sh
+++ ping.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-#PING=$(ping seznam.cz -w1)
-PING_adv=$( ping 10.0.0.6 -w1 | grep "ttl"  | cut -d " " -f7 | cut -d "=" -f2)
+PING=$1
+PING_adv=$( ping "$1" -w1 | grep "ttl"  | cut -d " " -f7 | cut -d "=" -f2)
 
 if [[ ${PING_adv} -gt 0 ]]; then
     echo "Internet dostupný"
